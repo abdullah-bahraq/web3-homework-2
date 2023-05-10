43d062273f0a2b1b1c90e1953ffa4bc402bbc917
Abdullah A. Bahraq <abdallahbhrag@gmail.com>
2023-05-10 17:16:05 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using _2.Models;
+
+namespace _2.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Calculate(double num1, double num2, string op)
+    {
+        double result;
+
+        switch (op)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num2 == 0 ? 0 : num1 / num2;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        ViewBag.Result = result;
+
+        return View("Index");
+    }
+}
